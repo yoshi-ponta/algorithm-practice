4d31e11eddb4de5b334b18ab86cc240913f84221
yoshimaru <yoshi0410777@gmail>
2025-06-25 16:50:30 +0900

    add:配列の要素の追加と削除

--- algorithm/01_sort_array.rb
+++
@@ -1,8 +0,0 @@
-#配列（Array）
-numbers = [1, 2, 3, 4, 5]
-
-#異なるデータ型の要素を混在させることも可能
-mixed_array = [1, "two", 3.0, [4, 5], { six: 6 }]
-
-p numbers
-p mixed_array
---
+++ chapter1/array_push.rb
@@ -0,0 +1,6 @@
+Animals = ["cat", "dog", "lion", "elephant", "panda"]
+p Animals
+#pushメソッドを使うと、配列の末尾に要素を追加することができる。
+#<<メソッドを使うことでも、pushと同様に配列の末尾に要素を追加することができる。
+Animals.push("Chameleon")
+p Animals
