44c0a03ab59aec3ba90bc1e4dc42910558a19f31
yoshimaru <yoshi0410777@gmail>
2025-08-09 11:41:45 +0900

    add:条件分岐の練習

---
+++ ruby/puractice14.rb
@@ -0,0 +1,20 @@
+# number = 1     1をnumberに代入
+# number == 1  # numberが1と等しいかを比較
+
+# if文による条件分岐
+number = rand(1..5)
+puts "あなたの順位は#{number}位です"
+if number == 1
+  puts "おめでとう！"  # 式が成り立った時の処理
+else
+  puts "あと少し"# 式が成りたたなかった時の処理
+end
+
+# if文による条件分岐
+number = rand(1..3) * 100
+puts "あなたの得点は#{number}ポイントです"
+if number == 300
+  puts "おめでとう！"
+else
+  puts "ざんねん！"
+end
