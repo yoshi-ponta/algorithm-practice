88ac2dffdf6bced694b4877bd6caaa87ddf03f28
yoshimaru <yoshi0410777@gmail>
2025-08-11 23:01:56 +0900

    add:西暦を表示するプログラム

--- ruby/practice15.rb
+++ ruby/practice15.rb
@@ -13,3 +13,18 @@ if dice >= 4
 elsif
   p "スライムから10のダメージを受けた"
 end
+
+
+seireki = rand(1926..1988)
+print "西暦#{seireki}年は"
+
+# 昭和年を計算する
+showa = seireki - 1925
+# 昭和年を出力する
+puts "昭和#{showa}年です。"
+
+seireki = rand(2019..2025)
+print "西暦#{seireki}年は"
+
+reiwa = seireki - 2018
+puts "令和#{reiwa}年です。"
