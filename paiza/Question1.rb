259ab2863102c37e9c8df32b1aab6db2af3d9318
yoshimaru <yoshi0410777@gmail>
2025-08-20 22:22:57 +0900

    add:mapメソッドを使用し、to_iメソッドで数字の新たな配列を作る

--- paiza/Question1.rb
+++ paiza/Question1.rb
@@ -1,3 +1,28 @@
+#問1
 a,b,c = gets.split.map(&:to_i)
 result = a - b + c
 puts result
+
+#問2
+S = gets.chomp
+if S == "paiza"
+  puts "YES"
+else
+  puts "NO"
+end
+
+#問3
+N = gets.to_i
+if N <= 100
+    puts "YES"
+else
+    puts "NO"
+end
+
+#問4
+A,B,C = gets.split.map(&:to_i)
+if (A * B) <= C
+    puts "YES"
+else
+    puts "NO"
+end
